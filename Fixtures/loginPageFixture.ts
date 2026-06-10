5a065ffee11e7cbbaeaa8616503aa2b24fa34342
kogilapalliarunkumar <arunreddyk2001@gmail.com>
2026-06-10 02:11:31 -0700

    Initial commit

---
+++ Fixtures/loginPageFixture.ts
@@ -0,0 +1,20 @@
+import { test as base } from '@playwright/test';
+import { Loginpage } from '../POM/loginPage';
+export{expect} from '@playwright/test';
+
+type myFixture = {
+    loginPage: Loginpage;
+};
+
+export const test = base.extend<myFixture>({
+    loginPage: async ({ page }, use) => {
+
+        const loginPage = new Loginpage(page);
+
+        await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+        await use(loginPage);
+
+        await page.waitForTimeout(3000);
+    },
+});
---
+++ POM/loginPage.ts
@@ -0,0 +1,35 @@
+import { Page, Locator, expect } from '@playwright/test';
+
+export class Loginpage {
+    readonly page: Page;
+    readonly usernameInput: Locator;
+    readonly passwordInput: Locator;
+    readonly loginButton: Locator;
+    readonly myinfo: Locator;
+    readonly errorMessage:Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.usernameInput = page.locator('//input[@name="username"]');
+        this.passwordInput = page.locator('//input[@name="password"]');
+        this.loginButton = page.locator('//button[@type="submit"]');
+        this.myinfo = page.locator('//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[6]/a/span');
+        this.errorMessage=page.locator('//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/p');
+
+    
+    }
+     async login(username:string,password:string)
+     {
+        await this.usernameInput.fill(username);
+        await this.passwordInput.fill(password);
+        await this.loginButton.click();
+     }
+    async verifyValidationLogin()
+    {
+        await expect(this.myinfo).toBeVisible();
+    }
+    async verifyinvalidLogin()
+    {
+        await expect(this.errorMessage).toBeVisible();
+    }
+}
